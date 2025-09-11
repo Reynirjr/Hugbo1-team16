b3adb7693c853fd4afc937c5e4c6e63798f439b4
Benjamín <brj46@hi.is>
2025-09-11 12:00:31 +0000

    aftur þar sem springboot var ekki að virka hja mer

--- OOPS/OOPS/src/main/java/org/example/oops/OopsApplication.java
+++ OOPS/OOPS/src/main/java/org/example/oops/OopsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OopsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OopsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OopsApplication.class, args);
+	}
 
 }
