180c8de67e329f032c8b6511ca22b2ec5cb7ef11
jonnishmurda <jjo1@hi.is>
2025-09-23 12:20:23 +0000

    manage menu bætt við, hægt að bæta við, breyta og eyða.

--- OOPS/OOPS/src/main/java/org/example/oops/repository/ItemRepository.java
+++ OOPS/OOPS/src/main/java/org/example/oops/repository/ItemRepository.java
@@ -2,8 +2,10 @@ package org.example.oops.repository;
 
 import org.example.oops.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
@@ -30,5 +32,14 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
                      WHERE m.id = :menuId AND i.id = :itemId
                      """)
        Item findByMenuIdAndItemId(Integer menuId, Integer itemId);
+
+       @Modifying
+       @Transactional
+       @Query("""
+                     DELETE FROM Item i
+                     WHERE i.id = :itemId
+                     AND i.section.menu.id = :menuId
+                     """)
+       int deleteByMenuIdAndItemId(Integer menuId, Integer itemId);
 
 }
